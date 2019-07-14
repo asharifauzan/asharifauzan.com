e635abfced0e3553bc42b8f8830536d2bae8695f
Fauzan Hibatullah Ashari <52328248+asharifauzan@users.noreply.github.com>
2019-07-14 22:08:28 +0700

    Add files via upload

---
+++ listku.py
@@ -0,0 +1,16 @@
+angka = [3, 100, 4, 6, 77, 22, 26]
+
+print "list original"
+print angka
+
+angka.append(500)
+angka.append(1004)
+angka.append(1)
+
+print "list setelah ditambah beberapa elemen"
+print angka
+
+angka.sort()
+
+print "list setelah disort berdasar nilai terkecil"
+print angka
