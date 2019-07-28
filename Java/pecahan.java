dd941b990b272f96978a53e1725a2587965f6ff9
Fauzan Hibatullah Ashari <52328248+asharifauzan@users.noreply.github.com>
2019-07-28 21:25:17 +0700

    Update pecahan.java

--- Java/pecahan.java
+++ Java/pecahan.java
@@ -7,32 +7,32 @@ public class pecahan {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Masukkan pembilang 1 : ");
+        System.out.print("Masukkan Pembilang 1 : ");
         pemb1 = input.nextInt();
-    
-        System.out.print("Masukkan penyebut 1 : ");
+
+        System.out.print("Masukkan Penyebut 1 : ");
         peny1 = input.nextInt();
-    
-        System.out.print("Masukkan pembilang 2 : ");
+
+        System.out.print("Masukkan Pembilang 2 : ");
         pemb2 = input.nextInt();
 
-        System.out.print("Masukkan penyebut 2 : ");
+        System.out.print("Masukkan Penyebut 2 : ");
         peny2 = input.nextInt();
 
         System.out.print("Masukkan Operasi | 1 = + | 2 = - | 3 = x | 4 = / | : ");
         oper = input.nextInt();
 
         xpeny = peny1 * peny2;
-        xpemb1 = (xpeny / peny1) * pemb1; 
-        xpemb2 = (xpeny / peny2) * pemb2; 
+        xpemb1 = (xpeny / peny1) * pemb1;
+        xpemb2 = (xpeny / peny2) * pemb2;
         tambah = xpemb1 + xpemb2;
         kurang = xpemb1 - xpemb2;
         kali = pemb1 * pemb2;
         bagi1 = pemb1 * peny2;
-        bagi2 = peny1 * pemb2;
+        bagi2 = pemb2 * peny1;
 
         if (oper == 1) {
-            System.out.println(pemb1 + "   " + pemb2 + "   " + (xpemb1 + xpemb2));
+            System.out.println(pemb1 + "   " + pemb2 + "   " + tambah);
             System.out.println("- + - = -");
             System.out.println(peny1 + "   " + peny2 + "   " + xpeny);
         }
@@ -40,24 +40,23 @@ public class pecahan {
         else if (oper == 2) {
             System.out.println(pemb1 + "   " + pemb2 + "   " + kurang);
             System.out.println("- - - = -");
-            System.out.println(peny1 + "   " + peny2 + "   " + xpeny);       
-         }
+            System.out.println(peny1 + "   " + peny2 + "   " + xpeny);
+        }
 
         else if (oper == 3) {
             System.out.println(pemb1 + "   " + pemb2 + "   " + kali);
             System.out.println("- x - = -");
-            System.out.println(peny1 + "   " + peny2 + "   " + xpeny);        
+            System.out.println(peny1 + "   " + peny2 + "   " + xpeny);
         }
 
         else if (oper == 4) {
             System.out.println(pemb1 + "   " + pemb2 + "   " + bagi1);
             System.out.println("- / - = -");
-            System.out.println(peny1 + "   " + peny2 + "   " + bagi2);       
+            System.out.println(peny1 + "   " + peny2 + "   " + bagi2);
         }
 
         else {
-            System.out.println(pemb1 + "   " + pemb2 + "   " + xpemb1+xpemb2);
+            System.out.println("INPUT YANG ANDA MASUKKAN SALAH!");
         }
-
     }
 }
